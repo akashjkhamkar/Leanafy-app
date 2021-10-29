e3eb59ce58e92cb50736a2f6a21a21f8dedf97c0
akash <akash.khamkar40@gmail.com>
2021-10-29 22:19:23 +0530

    added some style to pokemon divs

--- src/components/BoxPage.vue
+++ src/components/BoxPage.vue
@@ -4,23 +4,28 @@
             Pokemon Managment
         </span>
 
-        <div class="Pokemons">
+        <div id="Pokemons">
             <div 
-            class="Pokemon"
+            id="Pokemon"
             v-for="pokemon in pokemons"
             v-bind:key="pokemon.id">
-                <h1>{{pokemon.name}}</h1>
-                <img v-bind:src="pokemon.img" alt="">
-                <ul>
-                    <li 
-                    v-for="ability in pokemon.types"
-                    v-bind:key="ability.index">
-                        {{ability.name}}
-                    </li>
-                </ul>
+                <div>
+                    <h1>{{pokemon.name}}</h1>
+                    <img v-bind:src="pokemon.img" alt="">
+                </div>
+                
+                <div id="types">
+                    <h3>types</h3>
+                    <ul>
+                        <li 
+                        v-for="ability in pokemon.types"
+                        v-bind:key="ability.index">
+                            {{ability.name}}
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-
 
     </div>
 </template>
@@ -37,5 +42,34 @@
         display: flex;
         flex-direction: column;
         justify-content: center;
+    }
+
+    #Pokemons{
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        gap: 10px;
+        margin-top: 10px;
+    }
+
+    #Pokemon{
+        display: flex;
+        justify-content: space-around;
+        align-content: center;
+
+        border: 1px black solid;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    ul {
+        padding: 0;
+    }
+    
+    #types{
+        display: flex;
+        flex-direction: column;
+        justify-content: center ;
+        align-items: center;
     }
 </style>
